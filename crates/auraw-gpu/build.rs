1e9af1e6fa320911ff40d871bc714e1722edb3cf
Duecki <linus@kaiserkicker.de>
2026-08-11 13:30:48 +0200

    Fix gitea build

--- crates/auraw-gpu/build.rs
+++ crates/auraw-gpu/build.rs
@@ -13,6 +13,8 @@ fn main() {
         "tone_analysis.wgsl",
         "tonemap.wgsl",
         "scene_adjustments.wgsl",
+        "mask_effects/shared.wgsl",
+        "mask_effects/glow.wgsl",
         "mask_effects/neon.wgsl",
         "creative_effects.wgsl",
         "view_transform.wgsl",
